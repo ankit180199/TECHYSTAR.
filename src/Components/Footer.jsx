6e794d11770c5b7c3e054c1f13944e59621359f4
Ankit <janankit1810@gmail.com>
2022-11-01 00:48:31 +0530

    commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>
+             TechyStar   
+            </h1>
+            <p>
+        @all right reserved
+            </p>
+        </div>
+        <div>
+           <h5>Follow us</h5>
+         <div>
+            <a href ="http://youtube.com" target={"_blank"}>Youtube</a>
+            <a href ="http://instagram.com" target={"_blank"}>Instagram</a>
+            <a href ="http://github.com" target={"_blank"}>Github</a>
+         </div>   
+        </div>
+    </footer>
+  )
+}
+
+export default Footer;
